10470672495ac73301d2b4cb08de04339fcc5c34
jomin398(S.R) <jomin398@gmail.com>
2020-01-04 22:50:56 +0900

    Update getWifiInfo().js

--- Module/Bios/Network/Wifi/getWifiInfo().js
+++ Module/Bios/Network/Wifi/getWifiInfo().js
@@ -1,11 +1,10 @@
 function getWifiInfo() {
 /*출처남겨주세요 ㅎ
-* 뭐이건 오픈소스이라 양심껏 부탁드려요.
-* 와이파이 정보를 가져오는 기능입니다.
+* 이건 오픈소스이라 양심껏 부탁드려요.
+* 이문서는 와이파이 정보를 가져오는 기능입니다.
 * 참고로 이 문서는 차후 수정됩니다.
 * Api는 메신저봇 레거시 Api코드입니다.
-* 또한 이문서의 안드로이드 소스는 우리의 친구인
-* 개발자도우미 누리집에서 참고하였습니다.
+* 또한 이문서의 안드로이드 소스는 우리의 친구인 안드로이드 개발자도우미 누리집에서 참고하였습니다.
 * reference Link = https://developer.android.com/reference/android/net/wifi/WifiManager
 */
 
@@ -21,5 +20,6 @@ function getWifiInfo() {
         return String(wifiInfo.toString()).replace(/ /g, "").replace(/[,]/g, "\n");
     }
 }
-//사용법!
-replier.reply/*console.log*/(getWifiInfo());
+/*사용법!*/
+replier.reply(getWifiInfo());
+
