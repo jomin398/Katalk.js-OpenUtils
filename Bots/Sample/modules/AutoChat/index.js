b40a6f3b66c3f6469ba0bf8f1dee312a005d4c86
jomin398 <jomin398@gmail.com>
2020-02-19 23:48:53 +0900

    Updates

--- Bots/Sample/modules/AutoChat/index.js
+++ Bots/Sample/modules/AutoChat/index.js
@@ -2,35 +2,53 @@ module.exports={
 	DB:require("DB"),
     Ai:{
     	DB:require("DB"),
-        checkWord:function(que, msg) { //적당히 비슷한 말인지 비교
-            var data = msg.split(" "); //수신된 채팅의 어절들 중
-            for (var n = 0; n < data.length; n++) { //한 개 이상이 저장된 채팅들에 포함되어 있다면,
-                if (que.indexOf(data[n]) != -1) return true; //대강 비슷하다고 판단
+        checkWord: function(que, msg) { //적당히 비슷한 말인지 비교
+            var data = msg.split(" "); //수신된 채팅을 어절 단위로 나눔
+            var count = 0;
+            for (var n = 0; n < data.length; n++) { //저장된 채팅에 어절들이 몇 개 포함되어 있는지
+                if (que.indexOf(data[n]) != -1) {
+                    count++;
+                }
             }
-            return false; //아님 말고
+            return count; //반환
         },
         getReply:function(baseDir,room, msg) { //수신된 채팅에 대한 적당한 답변 반환
+            /* msg = msg.replace(/[^(가-힣ㄱ-ㅎㅏ-ㅣ0-9 )]/g, ""); //수신된 채팅 내용 중 한글, 띄어쓰기, 숫자를 제외하고 전부 삭제 */
             var data = this.DB.readData(baseDir, room); //저장된 채팅들을 불러옴
             if (data != null && msg.length !== 0) { //저장된 채팅이 없거나, 10% 확률이 터진게 아니면, 작동 안함
                 data = data.split("\n"); //냥
                 var result = []; //비슷한 말들이 들어갈 배열
-                for (var n = 0; n < data.length - 1; n++) { //적당하다 싶은 녀석들을
-                    if (this.checkWord(data[n], msg)) result.push(data[n + 1]); //배열에 추가
+                var max = 0; //최대 유사도(?) 값
+                for (var n = 0; n < data.length - 1; n++) { //저장된 채팅들 중 비슷하다 싶은 녀석들을 배열에 넣을건데,
+                    var count = this.checkWord(data[n], msg); //유사도(?)를 가져와서
+                    if (count > 0) { //유사한 채팅이라면,
+                        if (count > max) { //기존에 확인했던 녀석들보다 유사도가 높으면, 결과 배열 초기화 및 최대 유사도 값 변경
+                            max = count;
+                            result = [];
+                        }
+                    if (count == max) { //이미 유사도가 더 높은 말이 있다면, 저장 안함
+                        result.push(data[n + 1]); //배열에 추가
+                    }
                 }
-                if (result[0] != null) return result[Math.floor(Math.random() * result.length)]; //배열이 빈게 아니라면 아무거나 하나 반환
+            }
+            if (result[0] != null) return result[Math.floor(Math.random() * result.length)]; //배열이 빈게 아니라면 아무거나 하나 반환
             }
             return null; //일치하는게 없거나, 저장된 채팅이 없거나, 발동할 확률(?)이 아니면, null 반환
-        },
-        say:function(msg, replier) { //그냥 말하는 함수
+            },
+            say:function(msg, replier) { //그냥 말하는 함수
             replier.reply("[AI] " + msg); //앞에다가 이상한 문구 붙이는 용도
-        },
-        isValidData:function(msg) { //배울 만한 채팅인지 구분하는 함수
-            var noStudy = ["/","\n"]; //엔터가 포함된건 학습 X. 비속어 필터링 등도 여기다가 넣으면 이상한 말은 안배움
-            for (var n = 0; n < noStudy.length; n++) {
-                if (msg.indexOf(noStudy[n]) != -1) return false;
+            },
+            isValidData: function(msg) { //배울 만한 채팅인지 구분하는 함수
+                var invalids = ["#", "/"];
+                for (var n = 0; n < invalids.length; n++) {
+                    if (msg.charAt(0) == invalids[n]) return false; //특정 문자로 시작하는 것은 학습 X.    
+                }
+                var noStudy = ["\n", "//"]; //엔터가 포함된건 학습 X. 비속어 필터링 등도 여기다가 넣으면 이상한 말은 안배움
+                for (var n = 0; n < noStudy.length; n++) {
+                    if (msg.indexOf(noStudy[n]) != -1) return false;
+                }
+                return true;
             }
-            return true;
-        }
     },
     Chatprocess:function(room, msg, sender, isGroupChat, replier, imageDB, packageName, threadId, roomList, scriptName) {
         room = room.trim();
