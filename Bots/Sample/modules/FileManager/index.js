3e5a7c38530cdda03368c459fb0b49e090ef9ad7
jomin398 <jomin398@gmail.com>
2020-02-23 22:21:07 +0900

    Update

--- Bots/Sample/modules/FileManager/index.js
+++ Bots/Sample/modules/FileManager/index.js
@@ -1,4 +1,5 @@
 module.exports = {
+    sdcard : "",
     isModuleInstalled: function(FolderUrl) {
         if (FolderUrl !== undefined) {
             if (typeof FolderUrl === "string") {
@@ -66,54 +67,39 @@ module.exports = {
             /*C:\Users\USER\Documents\GitHub\*/
         }
     },
-    Savefile: function(savPath, msg) { //파일에 내용을 저장하는 함수
+    createDir:function(baseDir) { //배운 채팅들이 저장될 폴더를 만드는 함수
+        var folder = new java.io.File(baseDir); //File 인스턴스 생성
+        folder.mkdirs(); //폴더 생성
+    },
+    saveData:function(baseDir,name, msg) { //파일에 내용을 저장하는 함수
         try { //사실, 나도 어디서 긁어와서 이곳저곳에서 사용하는 거임
-            var file = new java.io.File(savPath);
-            if (!file.exists()) {
-                file.mkdirs()
-            }
+            var file = new java.io.File(baseDir+"/"+ name + ".txt");
             var fos = new java.io.FileOutputStream(file);
             var str = new java.lang.String(msg);
             fos.write(str.getBytes());
             fos.close();
-            Log.info("파일 작성 성공");
         } catch (e) {
             Log.debug(e + ", " + e.lineNumber);
         }
     },
-    SaveData:function(savPath, msg) { //파일에 내용을 저장하는 함수
-    try { //사실, 나도 어디서 긁어와서 이곳저곳에서 사용하는 거임
-        var file = new java.io.File(savPath);
-        var fos = new java.io.FileOutputStream(file);
-        var str = new java.lang.String(msg);
-        fos.write(str.getBytes());
-        fos.close();
-    } catch (e) {
-        Log.debug(e + ", " + e.lineNumber);
-    }
-},
-    CreateDir: function(savPath) { //배운 채팅들이 저장될 폴더를 만드는 함수
-        var folder = new java.io.File(savPath); //File 인스턴스 생성
-        folder.mkdirs(); //폴더 생성
-    },
-    ReadData: function(savPath) { //파일에 저장된 내용을 불러오는 함수
+    readData:function(baseDir, name) { //파일에 저장된 내용을 불러오는 함수
         try { //사실, 나도 어디서 긁어와서 이곳저곳에서 사용하는 거임
-        var file = new java.io.File(savPath);
-        if (!file.exists()) return null;
-        var fis = new java.io.FileInputStream(file);
-        var isr = new java.io.InputStreamReader(fis);
-        var br = new java.io.BufferedReader(isr);
-        var str = br.readLine();
-        var line = "";
-        while ((line = br.readLine()) != null) {
-            str += "\n" + line;
+            var file = new java.io.File(baseDir+"/" + name + ".txt");
+            if (!file.exists()) return null;
+            var fis = new java.io.FileInputStream(file);
+            var isr = new java.io.InputStreamReader(fis);
+            var br = new java.io.BufferedReader(isr);
+            var str = br.readLine();
+            var line = "";
+            while ((line = br.readLine()) != null) {
+                str += "\n" + line;
+            }
+            fis.close();
+            isr.close();
+            br.close();
+            return str;
+        } catch (e) {
+            Log.debug(e + ", " + e.lineNumber);
         }
-        fis.close();
-        isr.close();
-        br.close();
-        return str;
-    } catch (e) {
-        Log.debug(e + ", " + e.lineNumber);
-    }
     }
 }
